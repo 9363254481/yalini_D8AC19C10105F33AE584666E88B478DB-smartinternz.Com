3544270f0af57597f5800c3dfb7d6a26bf47e923
9363254481 <145186370+9363254481@users.noreply.github.com>
2023-09-16 16:37:12 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,5 @@
+n=int(input("enter any year"))
+If (n%4=0):
+Print "leap year"
+else:
+Print"Not a leap year"
---
+++ main.py
@@ -0,0 +1,7 @@
+def fact(n):       
+If n = = 0:
+     return 1
+  else :
+           return n*fact(n-1)
+Print(fact(0))
+Print(fact(5))
